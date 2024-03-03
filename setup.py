5d11e6a92b80973888923831bf51055fe39f1c91
Dzl666 <Deng_Zilong@outlook.com>
2024-03-03 17:28:33 +0100

    update setup and install it

--- setup.py
+++ setup.py
@@ -5,14 +5,17 @@ INSTALL_REQUIRES = [
     "numpy",
     "torch>=1.21",
 ]
+
+# package metadata
+# List of Python modules included in the package
 setup(
     name="plr_exercise",
     version="1.0.0",
-    author="Jonas Frey",
-    author_email="jonfrey@ethz.ch",
+    description="Package of PLR_exercise",
+    author="Zilong Deng",
+    author_email="dengzi@ethz.ch",
     packages=find_packages(),
     python_requires=">=3.7",
-    description="A small example package",
     install_requires=[INSTALL_REQUIRES],
     dependencies=[
         "https://download.pytorch.org/whl/torch-2.1.0+cu121-cp38-cp38-linux_x86_64.whl"
